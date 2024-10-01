b4bd8af05d9b589be0f34753981c2b3b187ce5fa
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-01 10:35:16 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,1 +1,12 @@
-
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('138.197.17.168', 'stokeyst_hw3user', '8wvo@?+uuhHV', 'stokeyst_hw3');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
