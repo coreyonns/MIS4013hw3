15bb7053887f873ffc3ba7feb6503f683020b6ce
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-02 13:56:10 -0500

    Update model-courses-by-instructors.php

--- model-courses-by-instructors.php
+++ model-courses-by-instructors.php
@@ -3,7 +3,7 @@ function selectCoursesByInstructor($iid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time
-FROM `course` c join Section s on s.course_id where s.instuctor_id=1");
+FROM `course` c join Section s on s.course_id where s.instructor_id=1");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
