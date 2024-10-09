216a3297952622fec81f4f3b889abdabfe953d73
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-09 13:42:59 -0500

    Update model-sections-by-course.php

--- model-sections-by-course.php
+++ model-sections-by-course.php
@@ -3,7 +3,7 @@ function selectSectionsByCourse($cid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time
-FROM `course` c join Section s on s.course_id where s.course_id=?");
+FROM `course` c join Section s on s.course_id=c.course_id where s.course_id=?");
         $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
